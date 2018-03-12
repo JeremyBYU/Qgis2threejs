bc97e2f0dc9a575fe1ebaf843c0de228aa451482
Jeremy Castagno <JeremyBYU@gmail.com>
2018-03-11 22:48:57 -0400

    Added custom javascript script

--- js/Qgis2threejs.js
+++ js/Qgis2threejs.js
@@ -346,8 +346,8 @@ limitations:
   };
 
   app.buildDefaultCamera = function () {
-    app.camera = new THREE.PerspectiveCamera(45, app.width / app.height, 0.1, 1000);
-    app.camera.position.set(0, -100, 100);
+    app.camera = new THREE.PerspectiveCamera(45, app.width / app.height, 0.1, 5000);
+    app.camera.position.set(0, -100, 150);
   };
 
   app.currentViewUrl = function () {
